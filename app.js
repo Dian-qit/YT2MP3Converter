03422e378296b67005b413baefa0c97563d96acb
Dian-qit <vincentquitoriano38@gmail.com>
2023-12-28 05:53:24 -0800

    5th test

--- app.js
+++ app.js
@@ -25,32 +25,31 @@ app.get("/", (req, res) => {
 
 app.post("/convert-mp3", async (req, res) => {
     const videoId = req.body.videoID;
-    if (
-    videoId === undefined ||
-    videoId === "" ||
-    videoId === null
-) {
-    return res.render("index", {
-        success: false,
-        message: "Please Enter A Video ID Niggah",
-    });
-} else {
-        const fetchAPI = await fetch(`http://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, {
-        "method" : "GET",
-        "headers": {
-        "x-rapidapi-key": process.env.API_KEY,
-        "x-rapidapi-host": process.env.API_HOST
-    }
-});
+
+    try {
+        if (!videoId) {
+            throw new Error("Please Enter A Video ID Niggah");
+        }
+
+        const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`, {
+            method: "GET",
+            headers: {
+                "x-rapidapi-key": process.env.API_KEY,
+                "x-rapidapi-host": process.env.API_HOST
+            }
+        });
 
         const fetchResponse = await fetchAPI.json();
 
-        if(fetchResponse.status === "ok")
-            return res.render("index", {success : true, song_title: fetchResponse.title, song_link : fetchResponse.link});
-        else
-            return res.render("index", {success : false, message : fetchResponse.msg})
+        if (fetchResponse.status === "ok") {
+            return res.render("index", { success: true, song_title: fetchResponse.title, song_link: fetchResponse.link });
+        } else {
+            return res.render("index", { success: false, message: fetchResponse.msg });
+        }
+    } catch (error) {
+        return res.render("index", { success: false, message: error.message });
     }
-})
+});
 
 //start the server 
 app.listen(PORT, () => {
